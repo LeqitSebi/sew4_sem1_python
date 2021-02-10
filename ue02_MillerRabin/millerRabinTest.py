9ee34363299d12d7f6fe47f28ebfa4f1a744c0d5
slanitsch <7225@htl.rennweg.at>
2021-02-10 08:36:38 +0100

    Finished Miller Rabin Test.

--- ue02_MillerRabin/millerRabinTest.py
+++ ue02_MillerRabin/millerRabinTest.py
@@ -1,3 +1,26 @@
-def testMR(n: int, k: int):
-    for i in k:
-        break
+import random
+
+
+def miller_rabin(n, k):
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+print(miller_rabin(36, 2))
